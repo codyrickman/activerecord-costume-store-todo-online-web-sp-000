e0d54152b0db38b37b65b8141735eb82f209b288
Cody Rickman <cody@creativemindstudios.com>
2019-11-06 12:21:40 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -12,11 +12,3 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
 
   end
 end
-1. name
-2. location
-3. theme
-4. price
-5. whether they're family friendly or not
-6. opening date
-7. closing date
-8. long description
